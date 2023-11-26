390f4fa0e56be1265f3b533e60635a002461c905
asangzz <asangzzillustration@gmail.com>
2023-11-26 20:22:54 +0530

    Added Carousel

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Carousel from '../components/Carousal'
 
 export default function Home() {
   return (
+    <>
+    <Carousel/>
     <div>Main Page</div>
+    </>
   )
 }
