680775daae808f6d9a9913506479d8b934f0c48e
asangzz <asangzzillustration@gmail.com>
2023-12-03 01:15:17 +0530

    check 1

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 
-const mongoDB = require('./db');
-const restaurants = require('./restaurants');
+// const mongoDB = require('./db');
+// const restaurants = require('./restaurants');
 
-mongoDB();
- //restaurants();
+// mongoDB(); 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.use(express.json())
-app.use('/api', require("./routes/createUser"))
+// app.use(express.json())
+// app.use('/api', require("./routes/createUser"))
 
 
 app.listen(4000)
