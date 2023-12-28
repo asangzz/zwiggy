cc1bcb1bfb2965a70a3d542b1fd7a2d008a3f50f
asangzz <asangzzillustration@gmail.com>
2023-12-29 02:44:48 +0530

    cloudinary Image upload and api integration done

--- src/view/RestaurantAddItem.js
+++ src/view/RestaurantAddItem.js
@@ -1,51 +1,79 @@
 
-import React, { useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function AddItem() {
 
-    const [foodCategory, setFoodCategory] = useState([]);
-   
-  
-    const loadData = async () => {
-      let response = await fetch('http://localhost:4000/api/fooditem',{
-        method:"GET",
-        header:{
-          'Content-Type':'application/json'
-        }
-      });
-      response = await response.json()
-  
-     // setFoodItem(response[0])
-      setFoodCategory(response[1])
-  
-      console.log(response[0], response[1])
-    }
-  
-    useEffect(()=>{
-      loadData()
-    },[])
 
- 
-   
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+  const uploadImage = () => {
+    const data = new FormData()
+    data.append("file", image)
+    data.append("upload_preset", "zwiggy")
+    data.append("cloud_name", "dtfwvp3gs")
+    fetch("  https://api.cloudinary.com/v1_1/dtfwvp3gs/image/upload", {
+      method: "post",
+      body: data
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        
+        setUrl(data.url)
+        setFormData({ ...formData, img: data.url });
+        console.log(url)
+      })
+      .catch(err => console.log(err))
+  }
 
-    const [formData, setFormData] = useState({
-      CategoryName: '',
-      name: '',
-      img: '',
-      options: [   ],
-      description: '',
-      RestaurantId: localStorage.getItem('restaurantID')
+
+
+
+
+  const [foodCategory, setFoodCategory] = useState([]);
+
+
+  const loadData = async () => {
+    let response = await fetch('http://localhost:4000/api/fooditem', {
+      method: "GET",
+      header: {
+        'Content-Type': 'application/json'
+      }
     });
+    response = await response.json()
+
+    // setFoodItem(response[0])
+    setFoodCategory(response[1])
+
+    console.log(response[0], response[1])
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+
+
+
+  const [formData, setFormData] = useState({
+    CategoryName: '',
+    name: '',
+    img: '',
+    options: [],
+    description: '',
+    RestaurantId: localStorage.getItem('restaurantID')
+  });
 
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    console.log(JSON.stringify(formData))
+
 
     let response;
     try {
-       response =  await fetch('http://localhost:4000/api/createfooditem', {
+      response = await fetch('http://localhost:4000/api/createfooditem', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -56,7 +84,7 @@ export default function AddItem() {
       // Handle success, e.g., show a success message or redirect
       console.log('Food item created successfully');
 
-      
+
 
 
     } catch (error) {
@@ -65,18 +93,18 @@ export default function AddItem() {
     }
 
     // console.log(itemdetails)
-   const json = await response.json()
+    const json = await response.json()
     // console.log(json)
     // if (!json.success) {
     //   alert("Enter Valid Credentails")
     // }
 
-    if (json.success) { 
+    if (json.success) {
       navigate(`/restaurantFoodItem/${localStorage.getItem("restaurantID")}`)
     }
   };
 
-  
+
 
   // const handleOptionChange = (index,key, e) => {
   //   const {  key , value } = e.target;
@@ -91,18 +119,18 @@ export default function AddItem() {
   // };
   const handleOptionChange = (index, key, value) => {
     setFormData((prevData) => {
-     const newOptions = [...prevData.options];
-      newOptions[index] = { [key] : value } ;
+      const newOptions = [...prevData.options];
+      newOptions[index] = { [key]: value };
 
       // let mergedOptions = newOptions.reduce((acc, option) => ({ ...acc, ...option }), {});
-        
+
       return {
         ...prevData,
-       // mergedOptions: mergedOptions,
+        // mergedOptions: mergedOptions,
         options: newOptions,
-        
+
       };
-    }); 
+    });
   };
 
   // const handleOptionChange = (index, key, value) => {
@@ -128,7 +156,7 @@ export default function AddItem() {
   const handleAddOption = () => {
     setFormData((prevData) => ({
       ...prevData,
-      options: [...prevData.options, {  }],
+      options: [...prevData.options, {}],
     }));
   };
 
@@ -138,9 +166,9 @@ export default function AddItem() {
   }
   return (
     <>
-    
+
       <div className='container'>
-      <h4 className='mt-3 mb-3'>Add a Food Item</h4>
+        <h4 className='mt-3 mb-3'>Add a Food Item</h4>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Item Name</label>
@@ -148,72 +176,91 @@ export default function AddItem() {
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
 
-          <div className="mb-3">
+          {/* <div className="mb-3">
             <label htmlFor="img" className="form-label">Image Url</label>
             <input type="text" className="form-control" name='img' value={formData.img} onChange={onChange} />
+          </div> */}
+
+          <div>
+            <input name='img' type="file" value={formData.url}
+            //onChange={(e) => setImage(e.target.files[0])}
+            onChange={(e) =>{
+              setImage(e.target.files[0])
+              // setFormData({ ...formData, img: url })
+            }
+
+               }
+            >
+
+            </input>
+            <button type="button" onClick={uploadImage}>Upload</button>
           </div>
- 
+          <div>
+            <h1>Uploaded image will be displayed here</h1>
+            <img src={url} />
+          </div>
+
 
           <label htmlFor="category" className="form-label">Category</label>
           <select
-  className="form-select"
-  name="CategoryName"
-  value={formData.CategoryName}
-  onChange={onChange}
->
-
-        <option value="">Select a category</option>
-        {foodCategory.map((category, index) => (
-          <option key={index} 
-        value={category.CategoryName}
+            className="form-select"
+            name="CategoryName"
+            value={formData.CategoryName}
+            onChange={onChange}
           >
-            {category.CategoryName}
-          </option>
-        ))}
-      </select>
-     <div className='mt-4'>
-      <label>
-      Add Varient
-        
-           {formData.options.map((option, index) => (
-          <div   className='mt-2'>
-            <input
-            type="text"
-            placeholder="Size"
-            value={    Object.keys(option)[0] || ''  }
-            onChange={(e) => handleOptionChange(index, e.target.value, option[Object.keys(option)[0]])}
-          />
-          
-          
-          <input
-            type="text"
-            placeholder="Price"
-            value={option[Object.keys(option)[0]] || ''}
-            onChange={(e) => handleOptionChange(index, Object.keys(option)[0], e.target.value)}
-          />
+
+            <option value="">Select a category</option>
+            {foodCategory.map((category, index) => (
+              <option key={index}
+                value={category.CategoryName}
+              >
+                {category.CategoryName}
+              </option>
+            ))}
+          </select>
+          <div className='mt-4'>
+            <label>
+              Add Varient
+
+              {formData.options.map((option, index) => (
+                <div className='mt-2'>
+                  <input
+                    type="text"
+                    placeholder="Size"
+                    value={Object.keys(option)[0] || ''}
+                    onChange={(e) => handleOptionChange(index, e.target.value, option[Object.keys(option)[0]])}
+                  />
+
+
+                  <input
+                    type="text"
+                    placeholder="Price"
+                    value={option[Object.keys(option)[0]] || ''}
+                    onChange={(e) => handleOptionChange(index, Object.keys(option)[0], e.target.value)}
+                  />
+                </div>
+              ))} </label>
           </div>
-        ))} </label>
-        </div>
-        
-       
-        <button className="mt-2 mb-3 btn btn-outline-success" type="button" onClick={handleAddOption }>
-          Add Option
-        </button>
-     
-      
+
+
+          <button className="mt-2 mb-3 btn btn-outline-success" type="button" onClick={handleAddOption}>
+            Add Option
+          </button>
+
+
           {/* <div className="mb-3">
             <label htmlFor="email" className="form-label">Price</label>
             <input type="number" className="form-control" name='price' value={itemdetails.price} onChange={onChange} />
           </div>  */}
-          
+
           <div className="mb-3">
             <label htmlFor="address" className="form-label">Description</label>
             <input type="text" className="form-control" name='description' value={formData.description} onChange={onChange} />
           </div>
           <button type="submit" className="btn btn-success">Submit</button>
-           <Link className="m-3 btn btn-danger" to="/login">
-                My Order List
-              </Link> 
+          <Link className="m-3 btn btn-danger" to="/login">
+            My Order List
+          </Link>
         </form>
       </div>
 
